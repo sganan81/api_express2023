4adfdea5d056f0d6d5db790a8cdd284990172bab
Sebastian Gañan <sganan81@gmail.com>
2023-08-25 21:58:24 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
+
+app.get('/home', function (req, res) {
+    res.json({text: 'Estamos en casa'});
+});
+
+app.listen(port, () =>{    
+    console.log(`App escuchando en el puerto ${port}`);
+});
